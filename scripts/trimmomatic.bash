7ab42d7b1f4a245e08f02811bbc9b49d65f919d0
Reed A. Cartwright <cartwright@asu.edu>
2022-03-29 15:29:31 -0700

    Add sbatch arguments to trimmomatic.bash

--- scripts/trimmomatic.bash
+++ scripts/trimmomatic.bash
@@ -1,3 +1,16 @@
+#!/bin/bash
+ 
+#SBATCH -N 1  # number of nodes
+#SBATCH -n 1  # number of "tasks" (default: allocates 1 core per task)
+#SBATCH -t 0-00:10:00   # time in d-hh:mm:ss
+#SBATCH -p serial       # partition 
+#SBATCH -q normal       # QOS
+#SBATCH -o slurm.%j.out # file to save job's STDOUT (%j = JobId)
+#SBATCH -e slurm.%j.err # file to save job's STDERR (%j = JobId)
+#SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
+#SBATCH --mail-user=%u@asu.edu # Mail-to address
+#SBATCH --export=NONE   # Purge the job-submitting shell environment
+
 # Load Module
 module purge
 module add trimmomatic/0.33
